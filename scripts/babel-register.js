5c70d90b153caa451339461028b149818e04f01c
kamijin_fanta <kamijin@live.jp>
2017-11-30 23:02:15 +0900

    goodbye npm, hello yarn & babel7

--- scripts/babel-register.js
+++ scripts/babel-register.js
@@ -2,12 +2,11 @@ var babel = require("../package.json").babel;
 var register = require("babel-register");
 var path = require("path");
 
-register({
+module.exports = register({
   extensions: [".js"],
-  // Only js files in the test folder but not in the subfolder fixtures.
-  only: /packages\/.+\/(test\/(?!fixtures\/)|src\/).+\.js$/,
+  // only: [
+  //   /packages\/.+\/(test\/(?!fixtures\/)|src\/).+\.js$/,
+  // ],
   babelrc: true,
   compact: true,
 });
-
-module.exports = register;
